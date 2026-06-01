e1e9a387d2ae06ab1a84fa0516e07e35ae2b56a6
Julio Estrada <168462108+JulioISTE@users.noreply.github.com>
2026-06-01 01:27:47 +0000

    Hemos salvado tio

--- src/practica5/p5a.asm
+++ src/practica5/p5a.asm
@@ -8,9 +8,23 @@ _start:
 	mov byte [msg], 'Z' ; imprime cadena msg terminada en valor nulo (0)
 	mov edx, msg		; edx = dirección de la cadena msg
 	call puts			;
+	push edx
+	push 3
+	call mover2
+	pop edx
+	call puts
 	mov	eax, 1	    	; seleccionar llamada al sistema para fin de programa
 	int	0x80        	; llamada al sistema - fin de programa
 
+
+mover2:
+	push ebp;
+	mov ebp, esp
+	mov eax, [ebp + 8]
+	mov edx, [ebp + 12]
+	mov byte [eax+edx], 'X'
+	pop ebp
+	ret 4
 section	.data
     msg	db  'abcdefghijklmnopqrstuvwxyz0123456789',0xa,0 
 
